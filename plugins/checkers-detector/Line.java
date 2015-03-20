dfeee3f4edbe7114f71ef76b21e225a1affb12a0
Ensis <pierre.ecormier@yahoo.fr>
2015-03-20 15:47:33 +0000

    add; geometry utils

---
+++ plugins/checkers-detector/Line.java
@@ -0,0 +1,17 @@
+public class Line {
+    private double _angle;
+    private double _dist;
+
+    public Line(double a, double d) {
+        _angle = a;
+        _dist = d;
+    }
+
+    public double getAngle() {
+        return (_angle);
+    }
+
+    public double getDist() {
+        return (_dist);
+    }
+}
