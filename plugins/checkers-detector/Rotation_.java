163832020089af0a06f64d25c04ec06d4e08ecad
Ghjuvan Lacambre <lacambrg.info@gmail.com>
2015-03-26 10:23:44 +0100

    Fixed Shear. Rotation is now three consecutive shears.

--- plugins/checkers-detector/Rotation_.java
+++ plugins/checkers-detector/Rotation_.java
@@ -8,32 +8,17 @@ public class Rotation_ implements PlugInFilter{
 
     public void run(ImageProcessor ip){
         Image i = new Image(ip);
-        Image j = rotate(i, 90);
-        Image k = rotate(i, 180);
-        Image l = rotate(i, 270);
-        Image m = rotate(i, 360);
-        Image n = rotate(i, 0);
+        Image j = rotate(i, 45);
         j.display();
-        k.display();
-        l.display();
-        m.display();
-        n.display();
     }
 
     public static Image rotate(Image img, double a){
-        a = a*Math.PI/180;
-        int w = img.getWidth(), h = img.getHeight();
-        Image r = new Image(w*2, h*2);
-        for(int i = 0; i < h; i++){
-            for(int j = 0; j < w; j++){
-                int x = (int)(j*Math.cos(a)+i*Math.sin(a))+w;
-                int y = (int)(-j*Math.sin(a)+i*Math.cos(a))+h;
-                if(x > 0 && x < w*2 && y > 0 && y < h*2){
-                    r.put(x, y, img.get(j, i));
-                }
-            }
-        }
-        return r;
+        a = Math.PI*a /180;
+        return Shear_.vShear(
+                Shear_.hShear(
+                    Shear_.vShear(img, -Math.tan(a/2)),
+                    Math.sin(a)),
+                -Math.tan(a/2));
     }
 
     public int setup(String args, ImagePlus imp) {
--- plugins/checkers-detector/Shear_.java
+++ plugins/checkers-detector/Shear_.java
@@ -8,17 +8,46 @@ import ij.process.*;
 public class Shear_ implements PlugInFilter{
 
     public void run(ImageProcessor ip){
-        Image i = hShear(new Image(ip), 0.5);
+        Image i = vShear(new Image(ip), -0.5);
         i.display();
     }
 
     public static Image hShear(Image img, double a){
-        Image result = new Image((int)(img.getWidth()+2*a), img.getHeight());
+        int offset = (int)(a*img.getHeight()+img.getWidth());
+        Image result;
+        if(offset > img.getWidth()){
+            result = new Image(offset,img.getHeight());
+            offset = 0;
+        } else {
+            offset = -offset;
+            result = new Image(img.getWidth() + offset, img.getHeight());
+        }
         for(int i = 0; i < img.getHeight(); i++){
             for(int j = 0; j < img.getWidth(); j++){
                 int newX = (int)(a*i+j);
                 if(newX >= 0 && newX < result.getWidth()){
                     result.put(newX, i, img.get(j, i));
+                }
+            }
+        }
+        return result;
+    }
+
+    public static Image vShear(Image img, double a){
+        int offset = (int)(a*img.getWidth()+img.getHeight());
+        Image result;
+        if(offset > img.getHeight()){
+            result = new Image(img.getWidth(),offset);
+            offset = 0;
+        } else {
+            offset = -(offset-img.getHeight());
+            result = new Image(img.getWidth(), img.getHeight() + offset);
+        }
+        for(int i = 0; i < img.getHeight(); i++){
+            for(int j = 0; j < img.getWidth(); j++){
+                int newY = (int)(a*j+i) + offset;
+                if(newY >= 0 && newY < result.getHeight()){
+                    result.put(j, newY, img.get(j, i));
                 }
             }
         }
