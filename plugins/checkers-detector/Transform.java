e763d2451d6ffd2db285ee09f1a1eae1ac40fe98
Ghjuvan Lacambre <lacambrg.info@gmail.com>
2015-05-09 17:09:46 +0000

    Fixed ArrayOutOfBondsException

--- plugins/checkers-detector/Transform.java
+++ plugins/checkers-detector/Transform.java
@@ -71,7 +71,7 @@ public class Transform {
                 Point frac = new Point(pt.x - px, pt.y - py);
                 double col = 0;
 
-                if (px < 0 || py < 0 || px > img.getWidth() || py > img.getHeight())
+                if (px < 0 || py < 0 || px >= img.getWidth() || py >= img.getHeight())
                     col = 0;
                 else if (px == img.getWidth() - 1 || py == img.getHeight() - 1)
                     col = img.get(px, py);
